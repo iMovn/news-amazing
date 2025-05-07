b18ee8d33bdac0c0763c2648a68254649f8ff6d7
hocvienimo <hocvienimo@gmail.com>
2025-05-07 14:54:45 +0700

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     images: {
-        domains: ["api.shenlong.cloud"],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'api.shenlong.cloud',
+            },
+        ],
     },
 };
 
